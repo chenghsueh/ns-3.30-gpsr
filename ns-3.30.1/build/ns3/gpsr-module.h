a2143d8757e10bdf8544a0dde43213400f7707a8
Cheng-Hsueh Lin <prince870226@gmail.com>
2020-06-22 15:46:06 +0800

    ns-3.30 with gpsr

---
+++ ns-3.30.1/build/ns3/gpsr-module.h
@@ -0,0 +1,15 @@
+
+#ifdef NS3_MODULE_COMPILATION
+# error "Do not include ns3 module aggregator headers from other modules; these are meant only for end user scripts."
+#endif
+
+#ifndef NS3_MODULE_GPSR
+    
+
+// Module headers:
+#include "gpsr-helper.h"
+#include "gpsr-packet.h"
+#include "gpsr-ptable.h"
+#include "gpsr-rqueue.h"
+#include "gpsr.h"
+#endif
